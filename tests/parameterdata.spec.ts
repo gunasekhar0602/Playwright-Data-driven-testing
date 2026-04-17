70d4edcd78c3a60a58b18564d1b34b0424beb1b6
gunasekhar0602 <gunabandla2000@gmail.com>
2026-04-17 14:30:00 +0530

    Refactor parameter data testing with added comments
    Refactored test case for parameter data testing with comments for clarity.

--- tests/parameterdata.spec.ts
+++ tests/parameterdata.spec.ts
@@ -7,24 +7,29 @@ import{test,expect}from'@playwright/test'
    await page.locator("//input[@value='Search']").click()
 
    await expect.soft(page.locator('h2 a').nth(0)).toContainText("laptop",{ignoreCase:true})
-}) */
+})*/
 
 
 
 
+
+// create an array and privide the items (test data )that which we need to verify
 const searchitems:string[]=["laptop","Gift card","smartphone","monitor"];
 
 for(const item of searchitems)
 {
     test(`Parameter data testing ${item}`,async({page})=>
 {
-   await page.goto('https://demowebshop.tricentis.com/');
-   await page.locator("//input[@id='small-searchterms']").fill(item)
-   await page.locator("//input[@value='Search']").click()
-
-   await expect.soft(page.locator('h2 a').nth(0)).toContainText(item,{ignoreCase:true})
-   await page.waitForTimeout(1000)
+    // Navigate to page
+    await page.goto('https://demowebshop.tricentis.com/');
+    // Locate the text box and fill the item
+    await page.locator("//input[@id='small-searchterms']").fill(item)
+    // Locate the search and click on it
+    await page.locator("//input[@value='Search']").click();
+    // Assertion whether the item is rflecting or not in the first item place
+    await expect.soft(page.locator('h2 a').nth(0)).toContainText(item,{ignoreCase:true})
+    await page.waitForTimeout(1000);
 })
-}
+
 
    
