167f90a9da08770582a6591b777e60fbefa29d8b
guna <gunabandla2000@gmail.com>
2026-05-16 11:49:33 +0530

    update

--- tests/parameter-EXCEL.spec.ts
+++ tests/parameter-EXCEL.spec.ts
@@ -2,48 +2,66 @@ import{test,expect} from'@playwright/test'
 
 import fs from'fs'
 
+// we need to import all the modules from XLSX
 import * as XlSX from'xlsx'
 
 // reading data from the file
+// file - workbook - sheets - rows and columns
 
-// file - workbbok - sheets - rows and columns
+// Capture the location of file and store it in a variable excelfilepath
+const excelpath='testdata/data1.xlsx'
 
-const excelpathfile='testdata/data1.xlsx'
+// Read the excel file and store in a variable wookbook
+// To read the excel file we need to use XLSX module
+const workbook=XlSX.readFile(excelpath)
 
-const workbook=XlSX.readFile(excelpathfile)
-
+// From workbook we need to grab all the sheets and store it in a variable sheetNames
+// SheetNames[0] is a method
 const sheetNames=workbook.SheetNames[0]
 
+// Returning the worksheet
 const worksheet=workbook.Sheets[sheetNames]
 
-// convert sheet into JSON
-
+// convert Excelsheet into JSON
+// in XLSX we are having a method called utils
+// inside utils we have a method called sheet_to_json and inside it pass worksheet
 const logindata:any=XlSX.utils.sheet_to_json(worksheet)
 console.log(logindata)
 
 test.describe("login data driven test",async()=>
 {
+    // using for loop
     for(const{email,password,validity} of logindata)
     {
         test(`login email and password ${email} ${password}`,async({page})=>
         {
+            // Navigate to the page
             await page.goto('https://demowebshop.tricentis.com/login')
+
+            // Entering email
             await page.locator("//input[@name='Email']").fill(email);
+
+            // Entering password
             await page.locator("//input[@name='Password']").fill(password);
+
+            // Click on the login button
             await page.locator("//input[@value='Log in']").click();
 
-
+            // Write a conditional statement
+            // if it is valid crendentials we should able to see the log out
             if(validity.toLowerCase()==="valid")
-            {
-                const logout= page.locator("//a[text()='Log out']");
-                await expect(logout).toBeVisible();
-            }
-            else
-            {
-                const errormessage=page.locator(".validation-summary-errors")
-                await expect(errormessage).toBeVisible()
+                {
+                    const logout= page.locator("//a[text()='Log out']");
+                    await expect(logout).toBeVisible();
+                }
+            // if it is invalid credentials we should able to see the error message
+                else
+                {
+                    const errormessage=page.locator(".validation-summary-errors")
+                    await expect(errormessage).toBeVisible()
 
-                await expect(page).toHaveURL("https://demowebshop.tricentis.com/login")
+                    // if it is invalid the url should not change
+                    await expect(page).toHaveURL("https://demowebshop.tricentis.com/login")
 
             }
         })
--- tests/parameterdata.spec.ts
+++ tests/parameterdata.spec.ts
@@ -5,7 +5,6 @@ test("Parameter data testing",async({page})=>
    await page.goto('https://demowebshop.tricentis.com/');
    await page.locator("//input[@id='small-searchterms']").fill("laptop")
    await page.locator("//input[@value='Search']").click()
-
    await expect.soft(page.locator('h2 a').nth(0)).toContainText("laptop",{ignoreCase:true})
 })
 
@@ -16,20 +15,25 @@ test("Parameter data testing",async({page})=>
 // create an array and privide the items (test data )that which we need to verify
 const searchitems:string[]=["laptop","Gift card","smartphone","monitor"];
 
+// write a for loop searchitems so that we can verify each item
 for(const item of searchitems)
 {
+    // Mention test name in backtick operator so that we can pass item value
     test(`Parameter data testing ${item}`,async({page})=>
 {
     // Navigate to page
     await page.goto('https://demowebshop.tricentis.com/');
+
     // Locate the text box and fill the item
-    await page.locator("//input[@id='small-searchterms']").fill(item)
+    await page.locator("//input[@id='small-searchterms']").fill(item);
+
     // Locate the search and click on it
     await page.locator("//input[@value='Search']").click();
-    // Assertion whether the item is rflecting or not in the first item place
-    await expect.soft(page.locator('h2 a').nth(0)).toContainText(item,{ignoreCase:true})
+
+    // Assertion whether the item is reflecting or not in the first item place
+    await expect.soft(page.locator('h2 a').nth(0)).toContainText(item,{ignoreCase:true});
     await page.waitForTimeout(1000);
 })
 
-
+}
    
